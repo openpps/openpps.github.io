29fa69dad3f44f0fdb6fe8df78dd27d79aec073b
OpenPPS <epicmap@icloud.com>
2019-03-15 03:18:16 +0800

    logo update

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,11 +21,9 @@
 		});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
-			}, 100);
-		});
+			}, 55);
 
 	// Nav.
 		var $nav = $('#nav');
